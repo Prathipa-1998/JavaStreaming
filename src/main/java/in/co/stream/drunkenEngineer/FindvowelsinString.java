7bb9d6071c8511dafd57fa551d6672932b5f565c
Prathipa-1998 <prathipathamarai98@gmail.com>
2024-09-29 18:10:32 +0530

    Update FindvowelsinString.java

--- src/main/java/in/co/stream/drunkenEngineer/FindvowelsinString.java
+++ src/main/java/in/co/stream/drunkenEngineer/FindvowelsinString.java
@@ -11,8 +11,7 @@ public class FindvowelsinString {
 		
 		long count=word.chars().filter((x)->{return (x=='a'||x=='e'||x=='i'||x=='o'||x=='u');}).count();
 		System.out.println("the vowels count in given sring is:"+count);
-		Map<String,Long> freq=word.chars().mapToObj(ch->(char)ch).collect(Collectors.groupingBy(Object::toString,Collectors.counting()));
-		freq.forEach((k,v)->System.out.println(k+v));
+		
 	}
 
 }
